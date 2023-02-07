e8c42613a7dde00cfb13d7dc72d26f26b0f1e83a
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-07 15:43:46 -0300

    remove items

--- src/components/products/productsItems.js
+++ src/components/products/productsItems.js
@@ -314,61 +314,6 @@ const productsItems = [
         ratings: 4,
         price: "7.5",
       },
-    
-      {
-        id: 41,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger1.png?alt=media&token=319dfbe9-462b-46ea-8f38-6ca7a20319e0",
-        name: "Burger Bistro",
-        ratings: 5,
-        price: "7.5",
-      },
-      {
-        id: 42,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger2.png?alt=media&token=b256ed7a-fe35-4051-9bf5-24f39b79f2b2",
-        name: "Smokin' Burger",
-        ratings: 5,
-        price: "12",
-      },
-      {
-        id: 43,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger3.png?alt=media&token=0ebe8311-6378-411b-9b6e-d7a6d2a106a2",
-        name: "Euphoria Burger",
-        ratings: 5,
-        price: "12",
-      },
-      {
-        id: 44,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger4.png?alt=media&token=f219c72c-22b5-484a-9135-34bbb84e7faa",
-        name: "Sassy Burger",
-        ratings: 5,
-        price: "12",
-      },
-      {
-        id: 45,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger5.png?alt=media&token=e8963c79-af46-4f33-a28d-059f98f464e3",
-        name: "Jazz Burger",
-        ratings: 5,
-        price: "12",
-      },
-      {
-        id: 46,
-        itemId: "buger01",
-        imgSrc:
-          "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger6.png?alt=media&token=f2b8febd-f0f8-48d7-9e1a-72d26b7bb36b",
-        name: "Healthy Buns",
-        ratings: 5,
-        price: "12",
-      },
       {
         id: 47,
         itemId: "pizza01",
