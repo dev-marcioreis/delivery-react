a0475a42ad5a531ebf174cedc2dc7a567e659ad6
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-08 10:47:15 -0300

    scroll cart item

--- src/components/home/cart/CartItems.jsx
+++ src/components/home/cart/CartItems.jsx
@@ -4,8 +4,131 @@ const CartItems = ( {name, img, qty, price} ) => {
   return (
     <>
         <section className='cart'>
-            <div className='cart-info'>
-                <h2>meu carrinho</h2>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
+            </div>
+            <div className="cart-item">
+                <div className="item-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="item-section">
+                    <span className='count'>x{qty}</span>
+                    <h3 className="item-name">{name}</h3>
+                    <div className="item-quantity">
+                        <div className="quantity">
+                            <button>-</button>
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-price">
+                    <span className='price'>R$ {price}</span>
+                </div>
             </div>
             <div className="cart-item">
                 <div className="item-image">
